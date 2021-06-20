22dadaa5aa541357c0acb1d63eb0f9c0c773a26d
Patrick Gauvin <gauvin.patrick@gmail.com>
2021-06-19 22:17:59 -0400

    show the keys!

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 A ntp server using NTP-KEY to authenticate
 
 # build
+
+```bash
 sudo docker build . --tag ntp 
+```
 
 # run
+
+Important: Always start from the same folder otherwize the keys will change.
+```bash
 sudo docker run --name ntp --rm \
                 -v `pwd`/config:/config \
                 --publish=123:123/udp \
                 ntp
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,12 @@
 cd config
 if [ ! -f "/config/ntp.keys" ]; then
   ntp-keygen -M
+  WARN='\033[0;33m'
+  NC='\033[0m' # No Color
+  echo "${WARN}=============================${NC}"
+  echo "${WARN}WARNING!${NC} New keys generated !"
+  echo "${WARN}=============================${NC}"
 fi
 
-echo using $(readlink -f /etc/ntp.keys)
-
-ntpd -g -d
-
+ntpd -g
+tail -f /config/ntp.keys
